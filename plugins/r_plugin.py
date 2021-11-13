6a8bfd94ea98b320d465805c2001c1afa27d683b
a-a-a-aa <78693043+a-a-a-aa@users.noreply.github.com>
2021-11-13 22:24:41 +0500

    Update r_plugin.py

--- plugins/r_plugin.py
+++ plugins/r_plugin.py
@@ -8,16 +8,20 @@ class R(commands.Cog):
         self.bot = bot
 
     @commands.command()
-    async def repeat(self, ctx, message="", num=1):
+    async def repeat(self, ctx, *message):
+        message = " ".join(message)
+        num = int(message.split('x')[-1]) | 1
+       
         if message.strip() == "":
-            await ctx.send(embed=await (embed_help("repeat [message] [number]")))
+            await ctx.send(embed=await(embed_help("repeat [message]x[number]")))
             return
         if num > 200:
-            await ctx.send(embed=await (embed_help("Must be less then 30")))
+            await ctx.send(embed=await(embed_help("Must be less then 30")))
         else:
+            msg_to_send = message.strip(f"x{num}")
             for _ in range(num):
-                await sleep(0.8)
-                await ctx.send(msgf(message))
+                await sleep(0.8)        
+                await ctx.send(msgf(msg_to_send))
 
 
 def setup(bot):
